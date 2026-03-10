19c7561c2117bd1e30010c747a67fe5b6d33b8f5
lekoooooo <levdidebashvili@gmail.com>
2026-03-10 23:57:59 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,3 @@
-// import type { Metadata } from "next";
-// import "./globals.css";
-
-// export const metadata: Metadata = {
-//   title: "XP Portfolio",
-//   description: "A Windows XP themed personal portfolio website.",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body className="antialiased overflow-hidden">{children}</body>
-//     </html>
-//   );
-// }
 import type { Metadata } from "next";
 import "./globals.css";
 
